a9ac58fb9c6941169976683328032ce7fbcaa498
Naïm Favier <n@monade.li>
2020-09-14 13:29:15 +0200

    Where do your SOCKS go when you lose them in th' WASHER?

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
-let
-  hosts = [ "wo" ];
-in {
+{
   description = "ncfavier's configurations";
 
   inputs = {
@@ -9,31 +7,34 @@ in {
     home-manager.url = github:rycee/home-manager/bqv-flakes;
   };
 
-  outputs = inputs: with inputs; {
-    nixosConfigurations = nixos.lib.genAttrs hosts (host:
-      nixos.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, ... }: {
-            networking.hostName = host;
+  outputs = inputs: with inputs;
+    let
+      hosts = [ "wo" ];
+    in {
+      nixosConfigurations = nixos.lib.genAttrs hosts (host:
+        nixos.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ({ pkgs, ... }: {
+              networking.hostName = host;
 
-            nix = {
-              package = pkgs.nixFlakes;
-              extraOptions = ''
-                experimental-features = nix-command flakes ca-references
-              '';
-              registry = {
-                config.flake = self;
-                nixos.flake = nixos;
+              nix = {
+                package = pkgs.nixFlakes;
+                extraOptions = ''
+                  experimental-features = nix-command flakes ca-references
+                '';
+                registry = {
+                  config.flake = self;
+                  nixos.flake = nixos;
+                };
               };
-            };
 
-            system.configurationRevision = self.rev;
-          })
-          home-manager.nixosModules.home-manager
-          (import (./hosts + "/${host}.nix"))
-        ];
-      }
-    );
-  };
+              system.configurationRevision = self.rev;
+            })
+            home-manager.nixosModules.home-manager
+            (import (./hosts + "/${host}.nix"))
+          ];
+        }
+      );
+    };
 }
