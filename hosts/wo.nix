c7d9064c0819588d0c9144e128bf4fc29676dde0
Naïm Favier <n@monade.li>
2020-10-03 03:35:24 +0200

    Future looks spotty. You will spill soup in late evening.

--- hosts/wo.nix
+++ hosts/wo.nix
@@ -7,15 +7,18 @@
     loader.grub = {
       enable = true;
       version = 2;
-      device = "/dev/vda";
+      device = "/dev/sda";
+      #device = "/dev/vda";
     };
 
-    initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+    initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "sd_mod" ];
+    #initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
   };
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/c4acae87-9dd9-4675-b5eb-2dd059955e8f";
+      device = "/dev/disk/by-uuid/7a2d7da5-0e2b-40d6-81ee-34734a4eaced";
+      #device = "/dev/disk/by-uuid/c4acae87-9dd9-4675-b5eb-2dd059955e8f";
       fsType = "ext4";
     };
   };
@@ -49,6 +52,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    tmux
     vim
   ];
 
@@ -56,16 +60,34 @@
     openssh.enable = true;
   };
 
+  #systemd.user.services.tmux-weechat = {
+  #  path = with pkgs; [ bash tmux ];
+  #  script = ''
+  #    tmux new-session -s weechat -n weechat -d -- bash -c 'date; exec bash'
+  #    tmux set -t weechat status off
+  #  '';
+  #  serviceConfig = {
+  #    Type = "forking";
+  #    RemainAfterExit = true;
+  #  };
+  #  restartIfChanged = false;
+  #};
+
   users.users.n = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD7KZW1RCBXJY1uDLbmaDUm50eshkv1rT8eK0JJXR3MfuCaJ/Kqrg547ZjczxED98Qy8A7d1BrIsOiKEoFVou+jCcjU19hlkQiMce3IZmYm0h6MOmZqB0MR6EGTlAgDfkiDMYqnAUGst4p2xqqmH/gM/UI2d5ZFrxAbK+PC4d7yMxs5QJkJ0buXRnbKL/LGRWwyUCV8UDzQ26kYufVyAhS2Iz2SvUSqca5BaJOzAPJ74CFScbICFK5nlsc2kHH35ZqK3f1Jxmbpi8ZwXUyxT+pFUClzY/s5H4w8c70ItvOyD3T0B+a8MF2Ft/c1kLFnHfYJd2FET+RZJQ5P+kXW+iZb n@monade.li"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXcKmcpfziEqVXmhYIJyZ03DOb5x7wcf+FxYUWewWeBS5g1MfWKw/FH1H0EQeJf6z0epc/0oN50AViqe1zBnUChGGF2xjNzGEpDPjHg0MuEDMboXBHDBbBRjb31S4T7pkZ72cCV06+bilWdYnXc0E7ND81BakmuBJHFH3DvjYXudFdhwLEtmXAVIOdLBlIStY6ZMkHojPOjnfYrREa7PfllrH0dqwQI/v1dU7E6ZHV5OK631HhcAFhySlu4jdo890czsEqwTkMSrPrgVXiiQipvFAavZvqB53d9J36BkSeVO3meqz2x9N6puXL1A/f+a2Suc5mfMUayFm35lE3sw1h tsu"
+    ];
   };
 
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
 
-    users.n = {
+    users.n = { config, ... }: {
       programs = {
         bash = {
           enable = true;
@@ -134,8 +156,11 @@
           sshKeys = [ "D10BD70AF981C671C8EE4D288F23BAE560675CA3" ];
         };
       };
+
+      #xdg.configFile."systemd/user/default.target.wants/tmux-weechat.service".source =
+      #  config.lib.file.mkOutOfStoreSymlink /etc/systemd/user/tmux-weechat.service;
     };
   };
 
-  system.stateVersion = "20.09";
+  system.stateVersion = "21.03";
 }
