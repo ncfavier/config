7681f2a9ee77f4bc7a2ed913d98297ae436d502f
Naïm Favier <n@monade.li>
2021-08-21 01:10:48 +0200

    make man-paths content-addressed

--- modules/environment/default.nix
+++ modules/environment/default.nix
@@ -1,4 +1,29 @@
-{ config, utils, pkgs, ... }: {
+{ lib, config, utils, pkgs, ... }: with lib; {
+  environment.etc."man_db.conf".text = let
+    manualPages = (pkgs.buildEnv {
+      name = "man-paths";
+      paths = config.environment.systemPackages;
+      pathsToLink = [ "/share/man" ];
+      extraOutputsToInstall = ["man"];
+      ignoreCollisions = true;
+    }).overrideAttrs (o: {
+      __contentAddressed = true; # avoids rebuilding the man cache every time nixos-version changes
+    });
+    manualCache = pkgs.runCommandLocal "man-cache" { } ''
+      echo "MANDB_MAP ${manualPages}/share/man $out" > man.conf
+      ${pkgs.man-db}/bin/mandb -C man.conf -psc >/dev/null 2>&1
+    '';
+  in mkForce ''
+    # Manual pages paths for NixOS
+    MANPATH_MAP /run/current-system/sw/bin /run/current-system/sw/share/man
+    MANPATH_MAP /run/wrappers/bin          /run/current-system/sw/share/man
+
+    # Generated manual pages cache for NixOS (immutable)
+    MANDB_MAP /run/current-system/sw/share/man ${manualCache}
+    # Manual pages caches for NixOS
+    MANDB_MAP /run/current-system/sw/share/man /var/cache/man/nixos
+  '';
+
   documentation = {
     dev.enable = true;
     man.generateCaches = true;
