3321a3fd203f2341ac5c58bd607746acda17c7c7
Naïm Favier <n@monade.li>
2022-01-21 14:15:50 +0100

    untest

--- modules/environment/default.nix
+++ modules/environment/default.nix
@@ -2,15 +2,15 @@
   documentation = {
     dev.enable = true;
     man.generateCaches = true;
-    # man.man-db.manualPages = (pkgs.buildEnv {
-    #   name = "man-paths";
-    #   paths = config.environment.systemPackages ++ config.hm.home.packages;
-    #   pathsToLink = [ "/share/man" ];
-    #   extraOutputsToInstall = ["man"];
-    #   ignoreCollisions = true;
-    # }).overrideAttrs (o: {
-    #   __contentAddressed = true; # avoids rebuilding the cache every time nixos-version changes
-    # });
+    man.man-db.manualPages = (pkgs.buildEnv {
+      name = "man-paths";
+      paths = config.environment.systemPackages ++ config.hm.home.packages;
+      pathsToLink = [ "/share/man" ];
+      extraOutputsToInstall = ["man"];
+      ignoreCollisions = true;
+    }).overrideAttrs (o: {
+      __contentAddressed = true; # avoids rebuilding the cache every time nixos-version changes
+    });
   };
 
   environment.localBinInPath = true;
