4670e9c0a0e971388d18aa6d3eaeab75b6715a89
Naïm Favier <n@monade.li>
2023-01-03 19:53:30 +0100

    swądach
    https://en.wiktionary.org/wiki/sw%C4%85dach

--- modules/localisation.nix
+++ modules/localisation.nix
@@ -1,7 +1,11 @@
-{
+{ pkgsMine, ... }: let
+  unicode15Locales = (pkgsMine "glibcLocales-unicode-15" "sha256-tomjrc051DQbjt2aNrmXltvwF407Z6aWV7mTVOAsstg=").glibcLocales; # FIXME
+in {
   hm.disabledModules = [ "config/i18n.nix" ];
 
   i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.glibcLocales = unicode15Locales;
+  cachix.derivationsToPush = [ unicode15Locales ];
 
   services.xserver = {
     layout = "fr,us,ru,gr";
