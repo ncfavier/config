b2775825575848fec8123ecaad6018855adedc9a
Naïm Favier <n@monade.li>
2023-01-05 18:00:48 +0100

    OSC 52!
    Works over SSH even without X11 forwarding.

--- modules/clipboard.nix
+++ modules/clipboard.nix
@@ -4,40 +4,47 @@
       environment.systemPackages = with pkgs; [
         xsel
         (writeShellScriptBin "clip" ''
-          clipin()  { xsel -bi; }
+          clipin() {
+            if [[ -v DISPLAY ]]; then
+              xsel -bi
+            else
+              printf '\e]52;c;%s\a' "$(base64)" >&2
+            fi
+          }
+
           clipout() { xsel -bo 2> /dev/null; }
 
           newline=0 edit=0 unfold=0
           while getopts :neu o; do case $o in
-              n) newline=1;;
-              e) edit=1;;
-              u) unfold=1;;
+            n) newline=1;;
+            e) edit=1;;
+            u) unfold=1;;
           esac done
           shift "$(( OPTIND - 1 ))"
 
           if (( edit )); then
-              tmpfile=$(mktemp) || exit
-              clipout > "$tmpfile"
-              ''${EDITOR:-vim} "$tmpfile"
-              clipin < "$tmpfile"
-              rm -f -- "$tmpfile"
+            tmpfile=$(mktemp) || exit
+            clipout > "$tmpfile"
+            ''${EDITOR:-vim} "$tmpfile"
+            clipin < "$tmpfile"
+            rm -f -- "$tmpfile"
           elif (( unfold )); then
-              clipout | tr -s '[:space:]' '[ *]' | clipin
+            clipout | tr -s '[:space:]' '[ *]' | clipin
           else
-              if [[ -t 0 ]] && (( ! $# )); then
-                  clipout | awk 1
+            if [[ -t 0 ]] && (( ! $# )); then
+              clipout | awk 1
+            else
+              if (( $# )); then
+                data=$*
               else
-                  if (( $# )); then
-                      data=$*
-                  else
-                      data=$(< /dev/stdin)
-                  fi
-                  if (( newline )); then
-                      printf '%s\n' "$data"
-                  else
-                      printf '%s' "$data"
-                  fi | clipin
+                data=$(< /dev/stdin)
               fi
+              if (( newline )); then
+                printf '%s\n' "$data"
+              else
+                printf '%s' "$data"
+              fi | clipin
+            fi
           fi
         '')
       ];
--- modules/tmux.nix
+++ modules/tmux.nix
@@ -15,11 +15,13 @@
       set -g history-limit 100000
       set -g mouse on
       set -g renumber-windows on
+      set -g set-clipboard on
       set -g set-titles on
       set -g set-titles-string '#T'
       set -g status-left ""
       set -g status-right "#S"
       set -g status-style ""
+      set -ga terminal-overrides ",xterm-256color:Ms=\\E]52;c;%p2%s\\7" # get mosh to behave https://gist.github.com/yudai/95b20e3da66df1b066531997f982b57b
       set -g window-status-current-format "#W"
       set -g window-status-current-style "bold fg=terminal"
       set -g window-status-format "#W"
