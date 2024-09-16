12dcc796355c3214fd3732f8476f012c09c955f9
Naïm Favier <n@monade.li>
2024-09-16 15:37:12 +0200

    u: add syncthing ID

--- machines/u.nix
+++ machines/u.nix
@@ -6,5 +6,6 @@
       ipv6 = "fd42::3:2";
       publicKey = "mnEKscultRQ+e6z87hUDUKPnZwmiCCx+gxl/Jmt8fUg=";
     };
+    syncthing.id = "5LJKMOL-FHRI6G6-JXPTSOI-CYX5RNE-QSC567O-UZJ4RPB-O3FPQVU-OLXBCQ6";
   };
 }
