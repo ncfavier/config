6aa73798d60df6b18c1a3fade075ea20a1b36444
Naïm Favier <n@monade.li>
2024-09-16 15:04:57 +0200

    add u

---
+++ machines/u.nix
@@ -0,0 +1,10 @@
+{
+  identity = {
+    isPhone = true;
+    wireguard = {
+      ipv4 = "10.42.3.2";
+      ipv6 = "fd42::3:2";
+      publicKey = "mnEKscultRQ+e6z87hUDUKPnZwmiCCx+gxl/Jmt8fUg=";
+    };
+  };
+}
