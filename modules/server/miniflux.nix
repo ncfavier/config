e799aacb580976459d6a7ef183cb536c39248554
Naïm Favier <n@monade.li>
2025-02-02 20:57:33 +0100

    отстёгивать
    https://en.wiktionary.org/wiki/%D0%BE%D1%82%D1%81%D1%82%D1%91%D0%B3%D0%B8%D0%B2%D0%B0%D1%82%D1%8C

--- modules/server/miniflux.nix
+++ modules/server/miniflux.nix
@@ -1,11 +1,11 @@
-{ lib, config, ... }: with lib; {
+{ lib, this, config, ... }: with lib; {
   secrets.miniflux = {};
 
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.secrets.miniflux.path;
     config = {
-      LISTEN_ADDR = "localhost:8072";
+      LISTEN_ADDR = "${this.wireguard.ipv4}:8072";
       BASE_URL = "https://flux.${my.domain}";
     };
   };
